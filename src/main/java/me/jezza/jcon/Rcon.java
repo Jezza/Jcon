1090acfe4718025078b38a9556976ce73581d848
Jezza <jezzadabomb@gmail.com>
2016-06-29 00:26:06 +0200

    Added the Rcon object, and with it the internals. It's now a functional Rcon API.

---
+++ src/main/java/me/jezza/jcon/Rcon.java
@@ -0,0 +1,17 @@
+package me.jezza.jcon;
+
+/**
+ * @author Jezza
+ */
+public class Rcon {
+
+	public Rcon() {
+	}
+
+	public static void main(String[] args) {
+	}
+
+	public static Rcon open(String host, int port, String password) {
+		return null;
+	}
+}
